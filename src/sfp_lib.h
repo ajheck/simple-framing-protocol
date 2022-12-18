ea9fc45d7fa41cf42ea60cacd8d2ecdf569d9703
ajonheck <ajonheck@gmail.com>
2022-12-18 09:21:04 -0600

    #4: Adds sfp_lib.c/.h

---
+++ src/sfp_lib.h
@@ -0,0 +1,10 @@
+/**
+ * @file sfp_lib.h
+ * @author ajheck
+ * @brief Public definitions and APIs for the core sfp library
+ * @version 0.1
+ * @date 2022-12-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
